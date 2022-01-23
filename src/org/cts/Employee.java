a0524ae84867849f48ae7fe5a94b9f7b1d436a05
veenapurushothamen <veenapurushothamen@gmail.com>
2022-01-23 04:23:47 -0800

    task done

---
+++ src/org/cts/Employee.java
@@ -0,0 +1,12 @@
+package org.cts;
+
+public class Employee {
+	public void add() {
+		System.out.println("add");
+        }
+      public static void main(String[] args) {
+		Employee e = new Employee();
+		e.add();
+
+}
+}
